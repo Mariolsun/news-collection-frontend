1add2b347b5b0b7e1132d01fe1104c5cfb517aa8
mariolsun <mariolsun@yandex.ru>
2020-05-02 18:43:14 +0300

    настроены кнопки в header

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
 import '../style/style.css';
+const navBar = document.querySelector('.header__navbar');
+const authButton =navBar.querySelector('.button_type_auth');
+const userButton = navBar.querySelector('.button_type_user');
+const savedArticlesPage = document.querySelector('.header__navbar-item_inactive-page');
+
+
+authButton.addEventListener('click', function(event) {
+  console.log('click on authbutton');
+  event.preventDefault();
+  authButton.classList.remove('header__navbar-item_visible');
+  userButton.classList.add('header__navbar-item_visible');
+  savedArticlesPage.classList.add('header__navbar-item_visible');
+})
+
+userButton.addEventListener('click', function(event) {
+  console.log('click on userbutton');
+  authButton.classList.add('header__navbar-item_visible');
+  userButton.classList.remove('header__navbar-item_visible');
+  savedArticlesPage.classList.remove('header__navbar-item_visible');
+})
