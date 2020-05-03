f31736a688c2a0639d5a9afec0ee4a9909d08db6
mariolsun <mariolsun@yandex.ru>
2020-05-03 14:26:44 +0300

    Заготовлено mobile menu для header

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,9 @@ const authButton =navBar.querySelector('.button_type_auth');
 const userButton = navBar.querySelector('.button_type_logout');
 const savedArticlesPage = document.querySelector('.header__navbar-item_inactive-page');
 
+const mobileMenuButton = document.querySelector('.button_type_mobile-menu');
+const mobileMenuClose = document.querySelector('.header__mobile-menu-close');
+const mobileNavBar = document.querySelector('.header__navbar_type_mobile');
 
 authButton.addEventListener('click', function(event) {
   console.log('click on authbutton');
@@ -18,4 +21,10 @@ userButton.addEventListener('click', function(event) {
   authButton.classList.add('header__navbar-item_visible');
   userButton.classList.remove('header__navbar-item_visible');
   savedArticlesPage.classList.remove('header__navbar-item_visible');
+})
+
+mobileMenuButton.addEventListener('click', function(event) {
+  console.log('click on mobile menu button');
+  mobileNavBar.classList.add('header__navbar_visible');
+  mobileMenuClose.setAttribute('display', 'inline');
 })
