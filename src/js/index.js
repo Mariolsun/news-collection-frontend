98aad0f86ad15a51f7b5aaec21c6c9948228e9c8
mariolsun <mariolsun@yandex.ru>
2020-05-05 22:51:51 +0300

    настроена grid сетка карточек новостей, добавлена кнопка показать еще

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,11 @@ const mobileMenuButton = document.querySelector('.button_type_mobile-menu');
 const mobileMenuClose = document.querySelector('.header__mobile-menu-close');
 const mobileNavBar = document.querySelector('.header__navbar_type_mobile');
 
+const findButton = document.querySelector('.lead__button');
+const foundArticles = document.querySelectorAll('.article');
+const articlesSection = document.querySelector('.articles');
+const showMoreBtn = document.querySelector('.articles__show-more-btn');
+
 authButton.addEventListener('click', function(event) {
   console.log('click on authbutton');
   event.preventDefault();
@@ -23,8 +28,34 @@ userButton.addEventListener('click', function(event) {
   savedArticlesPage.classList.remove('header__navbar-item_visible');
 })
 
+
 mobileMenuButton.addEventListener('click', function(event) {
   console.log('click on mobile menu button');
   mobileNavBar.classList.add('header__navbar_visible');
   mobileMenuClose.setAttribute('display', 'inline');
 })
+
+window.addEventListener('resize', function(event) {
+  console.log('resize event');
+})
+
+
+findButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  console.log('find button pressed');
+  articlesSection.classList.toggle('articles_visible');
+})
+
+showMoreBtn.addEventListener('click', function(event) {
+  event.preventDefault();
+  console.log('show more btn pressed');
+  let articlesToShow = 3;
+  for(let i = 0; i< foundArticles.length; i++) {
+
+    if(!foundArticles[i].classList.contains('article_visible') && articlesToShow > 0) {
+      foundArticles[i].classList.toggle('article_visible');
+      articlesToShow -= 1;
+    } else if(articlesToShow == 0) break;
+  }
+
+});
