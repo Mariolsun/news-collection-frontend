e3c792fabf95b718fb17856b8de272483d02c144
mariolsun <mariolsun@yandex.ru>
2020-05-06 12:53:17 +0300

    начаты классы article и user

---
+++ src/js/components/User.js
@@ -0,0 +1,40 @@
+export default class User {
+  constructor(logoutBtn, savedArticles, name, loggedIn = true) {
+    this.loggedIn = loggedIn;
+    this.logoutBtns = document.querySelectorAll('button_type_logout');
+    this.articles = savedArticles;
+    this.name = name;
+
+    this.updateArticles = this.updateArticles.bind(this);
+    this.updateUserName = this.updateUserName.bind(this);
+    this.addArticle = this.addArticle.bind(this);
+    this.removeArticle = this.removeArticle.bind(this);
+  }
+
+  updateUserName(newName) {
+    this.logoutBtns = document.querySelectorAll('button_type_logout');
+    this.name = name;
+    this.logoutBtns.forEach(button => {
+      button.textContent = this.name;
+    });
+  }
+
+  updateArticles(newArticles) {
+    this.articles = newArticles;
+  }
+
+  addArticle(newArticle) {
+    this.articles.push(newArticle);
+  }
+
+  removeArticle(articleId) {
+    this.articles.filter(article => article._id != articleId);
+  }
+
+  logout() {
+    this.loggedIn = false;
+    this.updateUserName('User');
+    this.updateArticles([]);
+  }
+
+}
