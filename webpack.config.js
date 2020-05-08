0d414e53fe65ce7327f397863cd944aafb2fe876
mariolsun <mariolsun@yandex.ru>
2020-05-08 15:21:11 +0300

    начата работа над шрифтами

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
       },
       {
         test: /\.(eot|ttf|woff|woff2)$/,
-        loader: 'file-loader?name=./vendor/[name].[ext]'
+        loader: 'file-loader?name=../vendor/fonts/[name].[ext]'
       }
     ]
   },
